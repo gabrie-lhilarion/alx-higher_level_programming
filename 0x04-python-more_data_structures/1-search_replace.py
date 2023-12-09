8674e1d923239ba5ed58e3aba56f9d561f04e264
garie-lhilarion <gabrielhilarion@gmail.com>
2023-12-09 03:58:49 +0100

    Change file mode

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    """
-    A function that replaces all occurrences
-    of an element by another in a new list
-    """
+  
     new_list = []
     for element in my_list:
         if element == search:
