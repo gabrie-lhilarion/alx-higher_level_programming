75901cdeabd2b56ab49bd06e5e8e61eecc678181
garie-lhilarion <gabrielhilarion@gmail.com>
2024-01-09 03:32:46 +0100

    Make corrections

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def lookup(obj):
-    return [attr for attr in dir(obj) if not callable(getattr(obj, attr)) or not attr.startswith("__")]
+    return [attr for attr in dir(obj)]
 
