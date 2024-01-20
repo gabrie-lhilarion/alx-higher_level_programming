fff8b8fa8d5c93922875c64efcf411590dcd8505
garie-lhilarion <gabrielhilarion@gmail.com>
2024-01-20 02:25:04 +0100

    Correct errors

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 
+"""Module documentation for the Base class."""
+
 class Base:
-    """ Private class attribute """
+    """
+    The Base class.
+
+    Private class attribute:
+        __nb_objects: Represents the number of objects created. Initialized to 0.
+
+    Public instance attribute:
+        id: Represents the identity of the object.
+
+    Class constructor:
+        __init__(self, id=None):
+            If id is not None, assigns the public instance attribute id with this argument value.
+            Otherwise, increments __nb_objects and assigns the new value to the public instance attribute id.
+    """
+
+    # Private class attribute
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -14,7 +31,8 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            """ Increment __nb_objects and assign the new value to the public instance attribute id """
+            # Increment __nb_objects and assign the new value to the public instance attribute id
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
 
