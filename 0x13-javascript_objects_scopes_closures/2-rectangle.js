0d1939d7625fdafcc168e8195f5f3271c88780b6
garie-lhilarion <gabrielhilarion@gmail.com>
2024-03-13 15:59:26 +0100

    Return this to create empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,11 @@
 
 class Rectangle {
   constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return this
+    }
     this.width = w
     this.height = h
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {}
-    }
   }
 }
 
