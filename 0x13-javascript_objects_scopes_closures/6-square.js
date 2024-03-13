3d0681692ab9cc751169db15a4f9df1c60bc2e66
garie-lhilarion <gabrielhilarion@gmail.com>
2024-03-13 22:14:26 +0100

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const MySquare = require('./5-square')
+
+class Square extends MySquare {
+  constructor(size) {
+    super(size, size)
+  }
+  
+charPrint(c) {
+  if (!c) {
+    c = 'X'
+  }
+
+  let row = c.repeat(this.width)
+    for (let i = 0; i < this.height; i++) {
+      console.log(row)
+    }
+  }
+}
+
+module.exports = Square
