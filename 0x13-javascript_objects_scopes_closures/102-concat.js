fd8a30e0ecffda99b8233b919a22f2e578fe9e0d
garie-lhilarion <gabrielhilarion@gmail.com>
2024-03-13 23:51:34 +0100

    Concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+
+const concatFiles = (File1, File2, destinationFile) => {
+  fs.readFile(File1, 'utf8', (err, data1) => {
+    if (err) {
+      console.error(`Error reading file ${File1}: ${err}`)
+      return
+    }
+
+    fs.readFile(File2, 'utf8', (err, data2) => {
+      if (err) {
+        console.error(`Error reading file ${File2}: ${err}`)
+        return
+      }
+
+      const concatenatedData = data1 + data2
+
+      fs.writeFile(destinationFile, concatenatedData, (err) => {
+        if (err) {
+          console.error(`Error writing to file ${destinationFile}: ${err}`)
+        }
+      })
+    })
+  })
+}
+
