34f0dcffc0b8893faf31a5a76dc0894730d219cc
garie-lhilarion <gabrielhilarion@gmail.com>
2024-03-13 23:54:26 +0100

    Concat files, correction

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,26 +3,33 @@
 const fs = require('fs')
 
 const concatFiles = (File1, File2, destinationFile) => {
-  fs.readFile(File1, 'utf8', (err, data1) => {
-    if (err) {
-      console.error(`Error reading file ${File1}: ${err}`)
-      return
-    }
-
-    fs.readFile(File2, 'utf8', (err, data2) => {
-      if (err) {
-        console.error(`Error reading file ${File2}: ${err}`)
-        return
-      }
-
-      const concatenatedData = data1 + data2
-
-      fs.writeFile(destinationFile, concatenatedData, (err) => {
+  
+    fs.readFile(File1, 'utf8', (err, data1) => {
         if (err) {
-          console.error(`Error writing to file ${destinationFile}: ${err}`)
+            console.error(`Error reading file ${File1}: ${err}`)
+            return
         }
-      })
+
+        fs.readFile(File2, 'utf8', (err, data2) => {
+            if (err) {
+                console.error(`Error reading file ${File2}: ${err}`)
+                return
+            }
+
+           
+            const concatenatedData = data1 + data2
+
+            fs.writeFile(destinationFile, concatenatedData, (err) => {
+                if (err) {
+                    console.error(`Error writing to file ${destinationFile}: ${err}`)
+                    return
+                }
+                
+            })
+        })
     })
-  })
 }
 
+// Usage: node concatFiles.js sourceFile1.txt sourceFile2.txt destinationFile.txt
+const [,, sourceFile1, sourceFile2, destinationFile] = process.argv;
+concatFiles(sourceFile1, sourceFile2, destinationFile);
