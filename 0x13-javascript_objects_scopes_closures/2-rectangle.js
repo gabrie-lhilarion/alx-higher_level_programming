daf6e9eda4ae9f46d933b7ca1fc92ef1ba9bf0bb
garie-lhilarion <gabrielhilarion@gmail.com>
2024-03-13 16:13:18 +0100

    Add comments

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
+/**
+ * Class:  Rectangle
+ * Returns empty object if either h or w id <= 0
+ */
+
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
