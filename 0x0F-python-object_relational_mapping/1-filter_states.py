9523df9b49133041321fc6d3b18c4943924b6068
garie-lhilarion <gabrielhilarion@gmail.com>
2024-03-19 06:21:10 +0100

    Remove more errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python3
 
-"""
-This script connects to a MySQL database and lists states
-starting with 'N' from the specified database.
-
-Usage:
-    python script.py <username> <password> <database>
-
-Arguments:
-    username (str): MySQL username.
-    password (str): MySQL password.
-    database (str): Name of the MySQL database containing
-    the states table.
-"""
-
 import MySQLdb
 import sys
 
 
 def list_states_starting_with_N(username, password, database):
     """
-    Connects to MySQL and lists states starting with 'N'
-    from the specified database.
+    Connects to MySQL, lists states starting with 'N' from the specified database.
 
     Args:
         username (str): MySQL username.
         password (str): MySQL password.
-        database (str): Name of the MySQL database containing
-        the states table.
+        database (str): Name of the MySQL database containing the states table.
 
     Returns:
-        None. Prints the list of states starting with 'N' to
-        the console.
+        None. Prints the list of states starting with 'N' to the console.
     """
     try:
+        # Connect to MySQL server
         connection = MySQLdb.connect(
             host="localhost",
             port=3306,
@@ -43,19 +27,18 @@ def list_states_starting_with_N(username, password, database):
         )
         cursor = connection.cursor()
 
-        sql_query = (
-            "SELECT * FROM states "
-            "WHERE name LIKE '{}' "
-            "ORDER BY id ASC"
-        ).format('N')
+        # Execute query
+        cursor.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-        cursor.execute(sql_query)
-
+        # Fetch all results
         states = cursor.fetchall()
 
+        # Display results
         for state in states:
             print(state)
 
+        # Close cursor and connection
         cursor.close()
         connection.close()
 
@@ -64,13 +47,10 @@ def list_states_starting_with_N(username, password, database):
 
 
 if __name__ == "__main__":
-    """
-    Entry point of the script. Connects to MySQL and lists
-    states starting with 'N'.
-    """
-
+    # Extract arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
+    # Call the list_states_starting_with_N function with provided arguments
     list_states_starting_with_N(username, password, database)
