53162bf498ec46ee38145885d6808ca42d273e88
garie-lhilarion <gabrielhilarion@gmail.com>
2024-03-22 05:16:17 +0100

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+"""
+Connects to MySQL, prints the State object with the specified
+name from the specified database.
+
+Returns:
+    None. Prints the id of the State object with the specified
+    name to the console, or "Not found" if not found.
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -22,36 +31,30 @@ def print_state_by_name(username, password, database, state_name):
         None. Prints the id of the State object with the specified
         name to the console, or "Not found" if not found.
     """
-    # Create engine to connect to MySQL server
+
     engine = create_engine(
         f'mysql://{username}:{password}@localhost:3306/{database}')
 
-    # Bind the engine to the Base class
     Base.metadata.bind = engine
 
-    # Create a session
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
-    # Query State object with the specified name
     state = session.query(State).filter(State.name == state_name).first()
 
-    # Display result
     if state:
         print(state.id)
     else:
         print("Not found")
 
-    # Close session
     session.close()
 
 
 if __name__ == "__main__":
-    # Extract arguments
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Call the print_state_by_name function with provided arguments
     print_state_by_name(username, password, database, state_name)
