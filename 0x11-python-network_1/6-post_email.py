2c3d32ba7f02b85e8473453364322c83d3ae2f08
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-03-28 20:33:56 +0100

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL and an email address,
+sends a POST request to the passed URL with the
+email as a parameter, and displays the body of
+the response.
+"""
+
+import requests
+import sys
+
+
+def send_post_request(url, email):
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    send_post_request(url, email)
