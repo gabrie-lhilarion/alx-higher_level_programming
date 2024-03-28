394707fb4514db58969af5572103a9ce71f30e90
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-03-28 18:55:25 +0100

    Add requirement

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 """
 This script takes in a URL, sends a request to the URL,
-and displays the value of the X-Request-Id variable
-found in the header of the response.
+and displays the value of the X-Request-Id variable found
+in the header of the response.
 """
 
 import urllib.request
 import sys
 
-url = sys.argv[1]
 
-req = urllib.request.Request(url)
+def fetch_x_request_id(url):
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        request_id = response.getheader('X-Request-Id')
+        return request_id
 
-with urllib.request.urlopen(req) as response:
-    request_id = response.getheader('X-Request-Id')
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script_name.py <url>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    request_id = fetch_x_request_id(url)
     print(request_id)
