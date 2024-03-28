d159bdf4505b7089cadf7afdcb674c68d77a3397
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-03-28 19:33:54 +0100

    Add requirement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 """
-This script fetches https://alx-intranet.hbtn.io/status using urllib.
+This script fetches https://alx-intranet.hbtn.io/status
+using urllib.
 """
 
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    print("Body response:")
-    print("    - type:", type(body))
-    print("    - content:", body)
-    print("    - utf8 content:", body.decode('utf-8'))
+def fetch_status():
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("    - type:", type(body))
+        print("    - content:", body)
+        print("    - utf8 content:", body.decode('utf-8'))
+
+
+if __name__ == "__main__":
+    fetch_status()
