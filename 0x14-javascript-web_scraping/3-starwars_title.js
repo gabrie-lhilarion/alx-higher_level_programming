ef9f8775aef64f8d6a8827c319a0ab6f42086334
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-04-23 19:51:33 +0100

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  console.log(`Title of Episode ${movie.episode_id}: ${movie.title}`);
+});
