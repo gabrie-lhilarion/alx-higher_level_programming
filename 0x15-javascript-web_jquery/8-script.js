18dd02edc9cdcef2d1cdfe31e51df16f07a63fd7
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-05-06 23:16:42 +0200

    Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $.ajax({
+        url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+        type: 'GET',
+        success: function (data) {
+            var movies = data.results;
+            $.each(movies, function (index, movie) {
+                $('#list_movies').append('<li>' + movie.title + '</li>');
+            });
+        },
+        error: function () {
+            $('#list_movies').append('<li>Error fetching movies</li>');
+        }
+    });
+});
