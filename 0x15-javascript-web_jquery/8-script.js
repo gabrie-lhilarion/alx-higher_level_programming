e304ba3db0969a7b56f27896a533f6c2d7acd984
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-05-07 00:38:23 +0200

    No jQuery - document loaded

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
         type: 'GET',
         success: function (data) {
-            var movies = data.results;
+            let movies = data.results;
             $.each(movies, function (index, movie) {
                 $('#list_movies').append('<li>' + movie.title + '</li>');
             });
